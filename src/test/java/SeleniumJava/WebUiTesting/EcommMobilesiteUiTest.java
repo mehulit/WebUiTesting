dde7b7c88d626a72826b85a4fd3e03e364d34f60
mehulit <mehulit@gmail.com>
2023-09-18 00:48:06 +0530

    method update by mehul

--- src/test/java/SeleniumJava/WebUiTesting/EcommMobilesiteUiTest.java
+++ src/test/java/SeleniumJava/WebUiTesting/EcommMobilesiteUiTest.java
@@ -1,12 +1,26 @@
 package SeleniumJava.WebUiTesting;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class EcommMobilesiteUiTest
 {
+	//System.setProperty("webdriver.driver.chrome", "E:/Mehul/Software/drivers/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
 	@Test
-	public void product()
+	public void mobileSiteUiTest()
 	{
-		System.out.println("productMobilesite");
+		System.setProperty("webdriver.driver.chrome", "E:/Mehul/Software/drivers/chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		driver.get("http://appium.io/docs/en/2.1/");
+		System.out.println(driver.getTitle());
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
 	}
 }
--- src/test/java/SeleniumJava/WebUiTesting/EcommWebsiteUiTest.java
+++ src/test/java/SeleniumJava/WebUiTesting/EcommWebsiteUiTest.java
@@ -1,12 +1,26 @@
 package SeleniumJava.WebUiTesting;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class EcommWebsiteUiTest
 {
+	//System.setProperty("webdriver.driver.chrome", "E:/Mehul/Software/drivers/chromedriver.exe")
+	WebDriver driver = new ChromeDriver();
 	@Test
-	public void product()
+	public void websiteUiTest()
 	{
-		System.out.println("productWebsite");
+		System.setProperty("webdriver.driver.chrome", "E:/Mehul/Software/drivers/chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/");
+		System.out.println(driver.getTitle());
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
 	}
 }
