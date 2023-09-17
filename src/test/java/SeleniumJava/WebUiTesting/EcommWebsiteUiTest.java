b37a8db987602c0087c30edf94e6aebacc053843
mehulit <mehulit@gmail.com>
2023-09-18 01:37:26 +0530

    EcommWebsiteUiTest.java file update by mehul

--- src/test/java/SeleniumJava/WebUiTesting/EcommWebsiteUiTest.java
+++ src/test/java/SeleniumJava/WebUiTesting/EcommWebsiteUiTest.java
@@ -2,25 +2,34 @@ package SeleniumJava.WebUiTesting;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class EcommWebsiteUiTest
 {
-	//System.setProperty("webdriver.driver.chrome", "E:/Mehul/Software/drivers/chromedriver.exe")
-	WebDriver driver = new ChromeDriver();
-	@Test
-	public void websiteUiTest()
+	
+	public static void main(String[] args)
 	{
 		System.setProperty("webdriver.driver.chrome", "E:/Mehul/Software/drivers/chromedriver.exe");
-		//WebDriver driver = new ChromeDriver();
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		WebDriver driver = new ChromeDriver(options);
+		
+		
+	}
+	
+	public void websiteUiTest(WebDriver driver)
+	{
+		
 		driver.get("https://www.selenium.dev/");
 		System.out.println(driver.getTitle());
 	}
 	
-	@AfterTest
-	public void tearDown()
+	public void tearDown(WebDriver driver)
 	{
 		driver.quit();
 	}
+	
 }
